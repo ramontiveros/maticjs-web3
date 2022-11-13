992773d6a0ffa7587338e941fc851a10357d106b
ramontiveros <rontiveros@sitek.mx>
2022-11-13 00:34:31 -0600

    Update web3_tx_to_matic_tx.ts

--- src/utils/web3_tx_to_matic_tx.ts
+++ src/utils/web3_tx_to_matic_tx.ts
@@ -1,5 +1,5 @@
 import { ITransactionData } from "@maticnetwork/maticjs";
-import { Transaction } from "web3/eth/types";
+import { Transaction } from "web3-eth/types";
 
 export const web3TxToMaticTx = (tx: Transaction) => {
     const maticTx: ITransactionData = tx as any;
